b8a6a1c1a1abe671bcfb330de5ea8e29f58f79b7
vinanamira <mzssdaru@gmail.com>
2025-04-15 16:47:27 +0700

    Edit env

--- config/mqttClient.js
+++ config/mqttClient.js
@@ -6,37 +6,37 @@ const MQTT_USERNAME = process.env.MQTT_USERNAME;
 const MQTT_PASSWORD = process.env.MQTT_PASSWORD;
 
 const options = {
-    clientId: 'mqtt-explorer-b5e48428',
+    clientId: 'mqtt-explorer-b59e5b32',
     username: MQTT_USERNAME,
     password: MQTT_PASSWORD,
-    reconnectPeriod: 30000, 
+    reconnectPeriod: 300000, 
     clean: true
 };
 
 const client = mqtt.connect(MQTT_BROKER_URL, options);
 
 client.on('connect', () => {
-    console.log(`✅ Connected to MQTT Broker at ${MQTT_BROKER_URL}`);
+    console.log(`Connected to MQTT Broker at ${MQTT_BROKER_URL}`);
 
-    client.subscribe('sensor/data', (err) => {
+    client.subscribe('tasmota/discovery', (err) => {
         if (!err) {
-            console.log('✅ Subscribed to topic: sensor/data');
+            console.log('Subscribed to topic: masmota/discovery');
         } else {
-            console.error('❌ Subscription error:', err);
+            console.error('Subscription error:', err);
         }
     });
 });
 
 client.on('message', (topic, message) => {
-    console.log(`📩 Received message from topic "${topic}": ${message.toString()}`);
+    console.log(`Received message from topic "${topic}": ${message.toString()}`);
 });
 
 client.on('error', (err) => {
-    console.error('❌ MQTT Connection Error:', err);
+    console.error('MQTT Connection Error:', err);
 });
 
 client.on('close', () => {
-    console.warn('⚠️ MQTT Connection Closed. Reconnecting...');
+    console.warn('MQTT Connection Closed. Reconnecting...');
 });
 
 module.exports = client;

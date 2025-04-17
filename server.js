ff456fe51780db6cf97223e6683da934e69a6010
vinanamira <mzssdaru@gmail.com>
2025-04-17 20:12:32 +0700

    Ganti total

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-require('./config/database');
+require('./config/mqttClient');
 
 app.use(express.json());
 app.use('/api', routes);

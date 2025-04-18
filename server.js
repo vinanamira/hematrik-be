45961f104a36ce12eb10af9c2e6c571603c89fa9
vinanamira <mzssdaru@gmail.com>
2025-04-18 17:42:02 +0700

    Tambah .get

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
 
-
 app.use(express.json());
 app.use('/api', routes);
+
+app.get('/', (req, res) => res.send('🟢 Hematrik Backend up and running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

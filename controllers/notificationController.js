a0bc7ac56205ba3412011b8b5ed0c9ce6aa08f35
vinanamira <mzssdaru@gmail.com>
2025-04-20 21:01:52 +0700

    Tambah notifikasi

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 require('../config/mqttClient');
+
 const db = require('../config/database');
 const cron = require('node-cron');
 
-// Fungsi utama: cek perangkat yang masih online dan simpan notifikasi
 const checkOnlineDevicesAndNotify = async () => {
   try {
     console.log('[CRON] Mengecek perangkat online untuk notifikasi jam 17.00...');
 
-    // Ambil perangkat yang statusnya online
     const [devices] = await db.execute(
       "SELECT device_id, device_name FROM Device WHERE status = 'online'"
     );
@@ -18,7 +17,6 @@ const checkOnlineDevicesAndNotify = async () => {
       return;
     }
 
-    // Simpan notifikasi untuk setiap perangkat online
     const notifPromises = devices.map(({ device_id, device_name }) => {
       const notif_message = `${device_name} masih menyala pada pukul 17.00`;
       return db.execute(
@@ -37,6 +35,9 @@ const checkOnlineDevicesAndNotify = async () => {
 
 cron.schedule('0 17 * * *', () => {
   checkOnlineDevicesAndNotify();
+}, {
+  scheduled: true,
+  timezone: 'Asia/Jakarta'
 });
 
 module.exports = { checkOnlineDevicesAndNotify };

2f3a9e1d28b23ed369d7544f7942ee1d1179d61b
vinanamira <mzssdaru@gmail.com>
2025-04-21 14:19:33 +0700

    Update router

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,8 +1,6 @@
-// controllers/notificationController.js
 const db   = require('../config/database');
 const cron = require('node-cron');
 
-// ① Cron job: simpan notifikasi tiap hari jam 17.00
 const checkOnlineDevicesAndNotify = async () => {
   try {
     const [devices] = await db.execute(
@@ -23,13 +21,11 @@ const checkOnlineDevicesAndNotify = async () => {
   }
 };
 
-// schedule cron job (pastikan ini di-require sekali di server.js)
 cron.schedule('0 17 * * *', () => {
   console.log('[CRON] Trigger notifikasi 17.00 WIB');
   checkOnlineDevicesAndNotify();
 }, { timezone: 'Asia/Jakarta' });
 
-// ② Ambil **satu notifikasi terbaru per device** (max 3)
 const getNotifications = async (req, res) => {
   try {
     const [devices] = await db.execute('SELECT device_id, device_name FROM Device');
@@ -50,7 +46,6 @@ const getNotifications = async (req, res) => {
   }
 };
 
-// ③ Tandai notifikasi sudah dikirim / dibaca
 const markAsSent = async (req, res) => {
   try {
     const { notif_id } = req.params;

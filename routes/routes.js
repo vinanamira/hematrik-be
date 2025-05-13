707ae8d49fe3e69a58497770017142055acd5eed
vinanamira <mzssdaru@gmail.com>
2025-05-13 18:19:14 +0700

    solving control

--- routes/routes.js
+++ routes/routes.js
@@ -8,13 +8,12 @@ const notifikasi = require('../controllers/notificationController');
 // Device
 router.get('/devices', device.getAllDevices);
 router.get('/devices/:device_id', device.getDeviceById);
-// router.post('/device/:device_id/control', device.controlDevice);
+router.post('/device/:device_id/control', device.controlDevice);
 
 // Post Control
-router.post('/device/:device_id/control', (req, res) => {
-  res.json({ message: 'test immediate' });
-});
-
+// router.post('/device/:device_id/control', (req, res) => {
+//   res.json({ message: 'test immediate' });
+// });
 
 // Electricity Log
 router.get('/logs', logListrik.getAllLogs);
